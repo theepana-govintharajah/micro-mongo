b910ef84aa22ab1653467e1230ed67aa38f93f1d
theepana govintharajah <theepanagovi01@gmail.com>
2023-01-26 12:18:27 +0530

    First commit

---
+++ organization-service/src/main/java/com/example/organizationservice/model/Organization.java
@@ -0,0 +1,22 @@
+package com.example.organizationservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(value = "Organizations")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Organization {
+    @Id
+    private int id;
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+}
